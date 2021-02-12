9f78a2c070725d8e49fa8ce9345a9ad89e2c7e28
HexFrag <mstracke@live.com>
2021-02-11 20:59:51 -0500

    Update OptionScoreSTUDY.ts
    fixed issue with aggregation type not matching chart

--- OptionScoreSTUDY.ts
+++ OptionScoreSTUDY.ts
@@ -1,20 +1,20 @@
 declare lower;
-input dper = AggregationPeriod.FIFTEEN_MIN;
+
 input p = FundamentalType.CLOSE;
 input length = 20;
 input averageType = AverageType.SIMPLE;
 
 def _rsi = if IsNaN(RSI()) then 0 else RSI();
 def step1 = If(_rsi <= 40, 1, 0);
-def _vol = if IsNaN(volume(period = AggregationPeriod.DAY)) then 0 else volume(period = AggregationPeriod.DAY);
+def _vol = if IsNaN(volume()) then 0 else volume();
 def step2 = If(_vol >= 100, 1, 0);
 
-def filled = if IsNaN(close(GetSymbol(), dper, PriceType.LAST)) then 0 else close(GetSymbol(), dper, PriceType.LAST);
-def lowerband = if IsNaN(MovingAverage(averageType, Fundamental(p, period = dper), length) - StDev(close, 20) * 2) then 0 else MovingAverage(averageType, Fundamental(p, period = dper), length) - StDev(close, 20) * 2;
+def filled = if IsNaN(close(priceType = PriceType.LAST)) then 0 else close(priceType = PriceType.LAST);
+def lowerband = if IsNaN(MovingAverage(averageType, Fundamental(fundamentalType = p), length) - StDev(close, 20) * 2) then 0 else MovingAverage(averageType, Fundamental(fundamentalType = p), length) - StDev(close, 20) * 2;
 
 def step3 = If(filled <= lowerband, 1, 0);
 
-def sma5 = if IsNaN(MovingAverage(averageType, Fundamental(p, period = dper), 5)) then 0 else MovingAverage(averageType, Fundamental(p, period = dper), 5);
+def sma5 = if IsNaN(MovingAverage(averageType,Fundamental(fundamentalType = p), 5)) then 0 else MovingAverage(averageType, Fundamental(fundamentalType = p), 5);
 def vw = vwap();
 def step4 = If(sma5 <= vw, 1, 0);
 
@@ -26,19 +26,20 @@ def spread = ask - bid;
 def step5 = If(spread >= 0.05, 1, 0);
 
 def step6 = If(filled equals bid, 1, 0);
-def iv = if IsNaN(imp_volatility(GetUnderlyingSymbol(), priceType = PriceType.LAST) * 100) then 0 else imp_volatility(GetUnderlyingSymbol(), priceType = PriceType.LAST) * 100;
+def iv = if IsNaN(imp_volatility(getUnderlyingSymbol(), priceType = PriceType.LAST) * 100) then 0 else imp_volatility(getUnderlyingSymbol(),priceType = PriceType.LAST) * 100;
 
 def step7 = if(iv <= 40, 1, 0);
 
 
-def dailyOpen = if IsNaN(open(period = AggregationPeriod.DAY)) then 0 else open(period = AggregationPeriod.DAY);
+def dailyOpen = if IsNaN(open(period = AggregationPeriod.DAY)) then (if isNaN(open()) then 0 else open()) else open(period = AggregationPeriod.DAY);
+
 def todaysGain = filled - dailyOpen;
 def step8 = If(todaysGain <= 0, 1, 0);
 
 def put = IsPut();
-
-def score = step1 + step2 + step3 + step4 + step5 + step6 + step7 + step8;
-plot s = score;
+def putcheck = if put then -8 else 0;
+def score = step1 + step2 + step3 + step4 + step5 + step6 + step7 + step8 + putcheck;
+plot plotscore = score;
 
 AddLabel(yes, if put then "PUT" else "CALL", if put then Color.RED else Color.GREEN);
 AddLabel(yes, "Score:" + score, if score >= 6 then if put then Color.RED else Color.GREEN else Color.RED);
